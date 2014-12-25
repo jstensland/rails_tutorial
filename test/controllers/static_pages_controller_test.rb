55d9494daf7d31489f70cf20db80c1eaa3f53652
Jared Stensland <jared.stensland@gmail.com>
2014-12-25 06:23:07 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,10 +14,9 @@ class StaticPagesControllerTest < ActionController::TestCase
   end
   
   test "should get about" do 
-  get :about
-  assert_response :success
-  assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
   
-
 end
