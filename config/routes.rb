c90308645fafb8b3fc27454c8e284a5470b0c060
Jared Stensland <jared.stensland@gmail.com>
2015-01-07 20:30:04 +0000

    Dropdown menu working but just finishing section 8.2.3

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users #adds all RESTful actions for Users resource
 
   # The priority is based upon order of creation: first created -> highest priority.
