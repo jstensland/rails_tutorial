8e7888585bd56c3704698a0fbfa6e6f73572f461
Jared Stensland <jared.stensland@gmail.com>
2015-01-30 02:01:09 +0000

    finished 11.3.4 deleting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,15 +55,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     
-    #Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-    
     #checks that it's the correct user trying to change their info
     def correct_user
       @user = User.find(params[:id])
